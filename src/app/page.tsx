3171883daa0c4b3f101cc33312c9c5e95d47c71c
ghabriel <ghsouza1013@gmail.com>
2025-05-08 00:43:41 -0400

    feat: implement RadioComponent and ButtonComponent for improved UI interaction

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,114 @@
-import Image from "next/image";
+'use client'
+import { GlobalIcon } from "@/components/GlobalIcon";
+import { useDebounce } from "@/hooks/useDebounce";
+import { HTMLAttributes, useEffect, useRef, useState } from "react";
+
+interface RadioComponentProps {
+  onClick: () => void
+  text: string
+  isSelected: boolean
+}
+
+const RadioComponent = ({
+  isSelected,
+  text,
+  onClick
+}: RadioComponentProps) => {
+  return (
+    <div className="flex flex-row gap-2 cursor-pointer" onClick={onClick}>
+      <GlobalIcon className="mt-0.5" iconName={isSelected ? "IoMdRadioButtonOn" : "IoMdRadioButtonOff"} color="var(--color-blue-primary)"/>
+      <p className="font-normal text-sm">{text}</p>
+    </div>
+  )
+}
+
+interface ButtonComponentProps {
+  onClick: () => void
+  text: string
+  className?: HTMLAttributes<HTMLDivElement>['className']
+}
+
+const ButtonComponent = ({
+  onClick,
+  text,
+  className
+}: ButtonComponentProps) => {
+  return (
+    <button className={`rounded-lg flex items-center justify-center cursor-pointer ${className ?? ''}`} onClick={onClick}>
+      <p className="font-semibold text-sm">{text}</p>
+    </button>
+  )
+}
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+  const [filterType, setFilterType] = useState<'tracked' | 'others'>('tracked')
+  const inputRef = useRef<HTMLInputElement>(null);
+  const debounce = useDebounce()
+  const [isFocused, setIsFocuses] = useState(false)
+
+  function handleInput(text: string) {
+    debounce(() => {
+      console.log({text})
+    })
+  }
+
+  const handleKeyDown = (e: KeyboardEvent) => {
+    if (e.code === 'KeyB') {
+      e.preventDefault(); 
+      inputRef.current?.focus();
+    }
+  };
+  
+  useEffect(() => {
+    window.addEventListener('keydown', handleKeyDown);
+    return () => window.removeEventListener('keydown', handleKeyDown);
+  }, []);
+
+  return (
+    <div className="flex min-h-screen flex-col">
+      <div className="bg-blue-20 flex min-w-screen h-14 items-center pl-6">
+        <p className="font-medium text-lg">Ghabriel Elias</p>
+      </div>
+      <div className="flex pl-9 pr-9 min-w-screen h-20">
+        <div className="border-b-1 border-b-blue-30 w-full h-full items-center flex">
+          <div className="w-1/2 flex flex-row">
+            <p className="font-semibold text-base">Lista</p>
+            <div className="flex flex-row gap-4 w-full justify-center">
+              <RadioComponent 
+                isSelected={filterType === 'tracked'}
+                onClick={() => setFilterType('tracked')}
+                text="Rastreados"
+              />
+              <RadioComponent 
+                isSelected={filterType === 'others'}
+                onClick={() => setFilterType('others')}
+                text="Outros"
+              />
+            </div>
+          </div>
+          <div className="w-1/2 flex flex-row justify-end gap-4">
+            <div className={`${isFocused ?  'border-white' : 'border-grey-border'} border-1 flex flex-row items-center rounded-lg h-10 w-64 pr-2`}>
+              <input
+                placeholder="Buscar por placa ou frota"
+                className="pl-2.5 pr-2.5 h-full w-full border-none outline-none font-light text-sm placeholder-grey bg-transparent"
+                ref={inputRef}
+                onChange={(ev) => handleInput(ev.target.value)}
+                onFocus={() => {
+                  setIsFocuses(true)
+                }}
+                onBlur={() => {
+                  setIsFocuses(false)
+                }}
+              />
+              {/* <div className="rounded-sm pl-1.5 pr-1.5 text-grey-secondary border-1">
+                <p className="font-semibold text-md text-grey-secondary">B</p>
+              </div> */}
+            </div>
+            <ButtonComponent text="Novo" onClick={() => {}} className="bg-blue-primary h-10 w-36"/>
+          </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      </div>
     </div>
   );
 }
