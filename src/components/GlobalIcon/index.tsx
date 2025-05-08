aed11bc61813a99e35946e0698adc08c09239ee4
ghabriel <ghsouza1013@gmail.com>
2025-05-08 00:42:56 -0400

    feat: add GlobalIcon component for dynamic icon rendering

---
+++ src/components/GlobalIcon/index.tsx
@@ -0,0 +1,71 @@
+import React, { HTMLAttributes } from 'react'
+import * as FaIcons from 'react-icons/fa'
+import * as MdIcons from 'react-icons/md'
+import * as AiIcons from 'react-icons/ai'
+import * as BsIcons from 'react-icons/bs'
+import * as BiIcons from 'react-icons/bi'
+import * as FiIcons from 'react-icons/fi'
+import * as IoIcons from 'react-icons/io'
+import * as GiIcons from 'react-icons/gi'
+import * as Icons from 'react-icons'
+
+type IconNameProps =
+  | keyof typeof FaIcons
+  | keyof typeof MdIcons
+  | keyof typeof AiIcons
+  | keyof typeof BsIcons
+  | keyof typeof BiIcons
+  | keyof typeof FiIcons
+  | keyof typeof GiIcons
+  | keyof typeof IoIcons
+
+interface IconPropsExtended {
+  iconName: IconNameProps
+  size?: number
+  testId?: string
+  color?: string
+  className?: HTMLAttributes<HTMLDivElement>['className']
+}
+
+
+const iconLibraries = [
+  FaIcons,
+  MdIcons,
+  AiIcons,
+  BsIcons,
+  BiIcons,
+  FiIcons,
+  GiIcons,
+  IoIcons
+]
+export const GlobalIcon: React.FC<IconPropsExtended> = ({
+  iconName,
+  size,
+  testId,
+  className,
+  color
+}) => {
+
+  const findIconLibrary = (): React.ComponentType<Icons.IconBaseProps> | null => {
+    for (const IconLib of iconLibraries) {
+      if (IconLib && (IconLib as any)[iconName]) {
+        return IconLib[iconName]
+      }
+    }
+    return null
+  }
+
+  const IconLib = findIconLibrary()
+
+  if (!IconLib) {
+    return null
+  }
+
+  return (
+    <IconLib
+      size={size}
+      className={className}
+      color={color}
+    />
+  )
+}
