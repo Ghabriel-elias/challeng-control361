f5199c54a6e1b0108cf4dc7f6a17e468f82401e8
ghabriel <ghsouza1013@gmail.com>
2025-05-10 14:17:11 -0400

    feat: add MapComponent for displaying vehicle locations with loading state

---
+++ src/components/MapComponent/index.tsx
@@ -0,0 +1,48 @@
+import Skeleton from "react-loading-skeleton"
+import { MarkerComponent } from "../MarkerComponent"
+import { GoogleMap } from "@react-google-maps/api"
+import { LocationVehicle } from "@/interfaces/vehicleInterfaces";
+
+interface MapComponentProps {
+  isLoaded: boolean;
+  vehiclesLocation: LocationVehicle[];
+  map: google.maps.Map | null;
+  onLoad: (map: google.maps.Map) => void;
+  handleClickOnTruck: (item: LocationVehicle) => void;
+  selectedVehicle: LocationVehicle | null;
+}
+
+export const MapComponent: React.FC<MapComponentProps> = ({
+  isLoaded,
+  vehiclesLocation,
+  map,
+  onLoad,
+  handleClickOnTruck,
+  selectedVehicle,
+}) => {
+  return (
+    <div className="mt-6 p-4 bg-blue-15 rounded-2xl border-blue-30 border-1">
+      <p className="font-medium text-md">Mapa rastreador</p>
+      {isLoaded && vehiclesLocation?.length ? (
+        <GoogleMap
+          mapContainerClassName="map-container"
+          center={map?.getCenter() || { 
+            lat: Number(vehiclesLocation?.at(0)?.lat), 
+            lng: Number(vehiclesLocation?.at(0)?.lng) 
+          }}
+          zoom={5}
+          onLoad={onLoad}
+          >
+          {vehiclesLocation?.map((item) => (
+            <MarkerComponent 
+              key={item?.id + item?.lat + item?.lng}
+              item={item} 
+              onClick={() => handleClickOnTruck(item)} 
+              isSelected={JSON.stringify(selectedVehicle) === JSON.stringify(item)}
+            />
+          ))}
+        </GoogleMap>
+      ) : <Skeleton width={'100%'} className="map-container" />}
+    </div>
+  )
+}
